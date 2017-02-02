22c78efb8c658b4d89a3c5d1067088d95a5c9e54
Shayde Nofziger <sln7755@rit.edu>
2017-02-02 11:12:27 -0500

    Fix config dir linter issues

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
-"use strict"
 // Use production config in production mode, development config otherwise
-module.exports = (process.env.NODE_ENV === "production") ?
-	require("./production") :
-	require("./development");
+module.exports = (process.env.NODE_ENV === 'production') ?
+  // TODO: Create production configuration file
+  // require('./production') :
+  {} :
+  require('./development');
