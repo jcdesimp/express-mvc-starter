84ad0540edd275974addc736109af9d70e6b4b94
Tina Howard <chh2538@rit.edu>
2017-02-02 10:39:16 -0500

    Fixed files according to lint

--- app/middleware/authentication.js
+++ app/middleware/authentication.js
@@ -1,5 +1,3 @@
-"use strict"
-
 const auth = require('../lib/authentication');
 
 /**
@@ -7,60 +5,51 @@ const auth = require('../lib/authentication');
  * is properly authenticated/logged in
  */
 function validateAuthentication(req, res, next) {
-	let authHeader = req.get("Authorization");
-	if(!authHeader) {
-		return res.status(401).send({
-			error: "Unauthorized",
-			message: "You are not authenticated."
-		})
-	}
+  const authHeader = req.get('Authorization');
+  if (!authHeader) {
+    return res.status(401).send({
+      error: 'Unauthorized',
+      message: 'You are not authenticated.',
+    });
+  }
 
-	let authMatch = authHeader.match(/Bearer\ (.*)$/);
+  const authMatch = authHeader.match(/Bearer (.*)$/);
 
-	if(!authMatch) {
-		return res.status(401).send({
-			error: "Unauthorized",
-			message: "You are not authenticated."
-		});
-	}
+  if (!authMatch) {
+    return res.status(401).send({
+      error: 'Unauthorized',
+      message: 'You are not authenticated.',
+    });
+  }
 
-	return auth.verifyToken(authMatch[1], (err, data) => {
-		if(err) {
-			if(err.name === 'TokenExpiredError') {
-				return res.status(401).json({
-					error: "TokenExpired",
-					message: "jwt expired"
-				});
-			}
+  return auth.verifyToken(authMatch[1], (err) => {
+    if (err) {
+      if (err.name === 'TokenExpiredError') {
+        return res.status(401).json({
+          error: 'TokenExpired',
+          message: 'jwt expired',
+        });
+      }
 
-			if(err.name === 'JsonWebTokenError') {
-				return res.status(401).json({
-					error: "NoToken",
-					message: "jwt must be provided"
-				});
-			}
-			console.log(err);
-			return res.status(401).json({
-				error: "Unauthorized",
-				message: "You are not authenticated."
-			});
-		}
+      if (err.name === 'JsonWebTokenError') {
+        return res.status(401).json({
+          error: 'NoToken',
+          message: 'jwt must be provided',
+        });
+      }
+      console.log(err);
+      return res.status(401).json({
+        error: 'Unauthorized',
+        message: 'You are not authenticated.',
+      });
+    }
 
 
-
-		return next();
-
-	});
-}
-
-/**
- * Middleware that attaches a user object to the request
- */
-function attachUser(req, res, next) {
-	// todo implement
+    return next();
+  });
 }
 
 
 module.exports = {
-	validateAuthentication
-}
+  validateAuthentication,
+};
