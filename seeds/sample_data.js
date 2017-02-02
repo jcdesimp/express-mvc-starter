7ec7131ead3cf5dac520e0f607364fc9ea6d4e5e
Shayde Nofziger <sln7755@rit.edu>
2017-02-02 11:25:31 -0500

    Fix seeds dir linter

--- seeds/sample_data.js
+++ seeds/sample_data.js
@@ -1,14 +1,13 @@
 // created using `knex seed:make sample_data`
 
-exports.seed = function(knex, Promise) {
+exports.seed = (knex, Promise) =>
   // Deletes ALL existing entries
-  return knex('people').del()
-    .then(function () {
-      return Promise.all([
+  knex('people').del()
+    .then(() =>
+      Promise.all([
         // Inserts seed entries
-        knex('people').insert({id: 1, make: 'rowValue1'}),
-        knex('people').insert({id: 2, make: 'rowValue2'}),
-        knex('people').insert({id: 3, make: 'rowValue3'})
-      ]);
-    });
-};
+        knex('people').insert({ id: 1, make: 'rowValue1' }),
+        knex('people').insert({ id: 2, make: 'rowValue2' }),
+        knex('people').insert({ id: 3, make: 'rowValue3' }),
+      ]),
+    );
